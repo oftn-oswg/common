418525043d600ccb1646b4ae523dc34a35fce3d7
Devin Samarin <eboyjr14@gmail.com>
2011-07-01 22:10:55 -0700

    [base64.js] Use Uint8Array for lookup tables, miscellaneous speed optimizations.

--- base64/base64.js
+++ base64/base64.js
@@ -2,25 +2,26 @@ var Base64;
 
 Base64 = {
 
-	rank: [
+	rank: new Uint8Array([
 		  62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1
 		, -1, -1,  0, -1, -1, -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9
 		, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
 		, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
 		, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
-	],
+	]),
 
-	alphabet: [
+	alphabet: new Uint8Array([
 		  65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80
 		, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99,100,101,102
 		,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118
 		,119,120,121,122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 43, 47
-	],
+	]),
 
 	encode: function(binary) {
 		var
 			  len = binary.length
-			, buffer = new Uint8Array((len / 3) * 4 | 0)
+			, alphabet = Base64.alphabet
+			, buffer = new Uint8Array((len / 3 + 1) * 4 | 0)
 			, ip = 0
 			, op = 0
 			, ibuf = [0, 0, 0]
@@ -42,15 +43,15 @@ Base64 = {
 			}
 			
 			if (bs) {
-				buffer[op++] = Base64.alphabet[ibuf[0] >> 2];
+				buffer[op++] = alphabet[ibuf[0] >> 2];
 				buffer[op++] =
-					Base64.alphabet[((ibuf[0] & 0x03) << 4) |
+					alphabet[((ibuf[0] & 0x03) << 4) |
 						((ibuf[1] & 0xf0) >> 4)];
 				buffer[op++] = (bs > 1 ?
-					Base64.alphabet[((ibuf[1] & 0x0f) << 2) |
+					alphabet[((ibuf[1] & 0x0f) << 2) |
 						((ibuf[2] & 0xc0) >> 6)] : 61);
 				buffer[op++] = (bs > 2 ?
-					Base64.alphabet[ibuf[2] & 0x3f] : 61);
+					alphabet[ibuf[2] & 0x3f] : 61);
 			}
 		}
 		
@@ -61,6 +62,7 @@ Base64 = {
 		var
 			  len = string.length
 			, buffer = new Uint8Array(len / 4 * 3 | 0)
+			, ranks = Base64.rank
 			, i = 0
 			, outptr = 0
 			, last = [0, 0]
@@ -71,8 +73,8 @@ Base64 = {
 		
 		while (len--) {
 			code = string.charCodeAt(i++);
-			rank = Base64.rank[code-43];
-			if (rank !== -1 && rank !== undefined) {
+			rank = ranks[code-43];
+			if (rank !== 255 && rank !== void 0) {
 				last[1] = last[0];
 				last[0] = code;
 				save = (save << 6) | rank;
